24967c7395e8cf248df6cab35d88d59beef5f8aa
wee3200 <hikariwee@gmail.com>
2015-09-25 19:32:39 +0800

    update new cardpack

--- bannerChange.js
+++ bannerChange.js
@@ -2,7 +2,7 @@ function bannerChange() {
 	var e = document.getElementById("slt_dMethod");
 	var elementUp = e.options[e.selectedIndex].value;
 	if(elementUp == 8) {
-		document.getElementById('cotentbanner').innerHTML = '<img src="images/banner_21341_4y0znfsl.png"/>';
+		document.getElementById('cotentbanner').innerHTML = '<img src="images/banner_21351_5lwtgff0.png"/>';
 	} else {
 		document.getElementById('cotentbanner').innerHTML = '<img src="images/banner_21331_3nfedxu2.png"/>';
 	}
